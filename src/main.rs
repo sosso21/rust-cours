252c5bdd579b05f05da9f669dcf432d5471ea0b4
sosso21 <sofianetop21.st@gmail.com>
2022-12-01 16:11:26 +0100

    :camera_flash: (feat): function + match

--- src/main.rs
+++ src/main.rs
@@ -1,120 +1,59 @@
-#![deny(clippy::all)]
-
-/*
- * hello
-  ! hallo
-  // hello
-? hallo
-TODO : hallo
-*/
 fn main() {
-    let name: &str = "sofiane";
-    let username: &str = "Gherab";
-    let letter: char = 'é';
-
-    let age: i32 = 24;
-
-    println!(
-        "Hello, world! ; my name is {} {} and I have  {} years old  , my favorite char is  {} ",
-        name, username, age, letter
-    );
-
-    // ?  ============================================
-    // * COURs 2
-    // ?  ============================================
-
-    // ? les variables mutable
-    let mut number: u32 = 1;
-    number += 1; // TODO : on elui ajouter +1
-
-    // ? les pointeur
-    let first_number: u32 = number;
-    let first_number: u32 = first_number - 1;
-    print!(" my first number id : {} ", first_number);
-
-    // ? ============================================
-    // * Rust : 3 - Types des variables
-    // ?  ============================================
+    // ? ==============================
+    // ? Cours 8
+    // ? ==============================
 
     /*
-    * INTEGERS :
-          ?  we can use :
-                          i8 , i16 , i32 , i64 , i128
-                          u8 , u16 , u32 , u64 , u128
-                          iSize , uSize
+    * Opntion
+     @return   :  None | Some
 
-          ?  signification :
-                            *  i ->   ]-∞;+∞[
-                            *  u ->   ]0;+∞[
-                            * iSize -> array size ]-∞;+∞[
-                            * uSize -> array size ]0;+∞[
+    */
 
-     */
-    let int_number: i32 = 123;
-    let int_number_negative: i32 = -123;
-    let this_number_is_positive: u32 = 123;
+    let my_name: Option<String> = None;
 
-    print!(
-        "{} -  {} - {} ",
-        int_number, int_number_negative, this_number_is_positive
-    );
+    match my_name {
+        Some(name) => println!("my name is  {}", name),
+        None => println!("I have no name"),
+    };
 
-    /*
-    * FLOAT :
-    ?  We can use :   f32 , f64
-     */
-    let pi1: f32 = 3.1415926;
+    let pont = Some(10);
 
-    /*
-     * BIOLEAN
-     */
-    let is_subscribe: bool = true;
-    let is_not_sub: bool = false;
+    let ratio: f32 = match pont {
+        Some(p) => {
+            let ratio: f32 = p as f32 / 20 as f32;
+            println!(" {} / {} = {} ", p, 20, ratio);
+            // ? [1] we can return Some(ration)
+            ratio
+        }
+        None => 0 as f32, // ? [1]  but we should return None
+    };
+    println!(" ratio {}", ratio);
 
-    /*
-     * CHAR
-     */
-    let my_char = 'à';
+    let age = 24;
 
-    /*
-     * STRING
-     */
-    let my_string: &str = "hello word";
+    match age {
+        0 => println!("tu viens de naitre "),  // if 0
+        100 => println!("tu va creuver papi"), // if 100
+        _ => println!("tva bosser"),           // = else
+    };
 
-    /*
-     * TUPLE
-     */
-    // let (first: i32 , second : i32 , third : i32 ) =( 1,2,3 );
-    let winner: (i32, i32, i32) = (1, 2, 3);
-    let (fst, snd, trd) = (1, 2, 3);
-    let the_fst = winner.0;
+    // ? ==============================
+    // ? Cours 9 : les functions
+    // ? ==============================
 
-    /*
-     * array
-     */
-    // let numbers_array: [u8; 5] = [0, 1, 2, 3, 4];
-    let numbers_array: [u8; 5] = [1; 5];
-    let array_length: usize = 4;
-    let _the_last_el_of_array: u8 = numbers_array[4];
-    // or
-    let _the_last_el_of_array_2: u8 = numbers_array[array_length];
+    // ? [2]  this function return nothing
+    nothing(12);
 
-    /*
-    * PARSING
+    // ? [2]  this function return  hello
+    let message = hello();
+    println!("message : {}", message);
+}
 
-     */
+// ? [2] : -> () : mean that the function return nothing
+fn nothing(x: i32) -> () {
+    println!("{}", x)
+}
 
-    let my_number_string: &str = "21";
-
-    let _my_number_after_parsing = my_number_string
-        .parse::<i32>() // on peut s'arreter là
-        .expect("this is not a number"); // return an error of it's not number
-
-    // * PARSE Des type proches en eux
-    let array_2: [&str; 4] = ["hello"; 4];
-    let index: i32 = 1;
-    let index2: usize = index as usize;
-    print!("{}", array_2[index as usize])
-
-    //--
+fn hello() -> &'static str {
+    return "hello";
 }
