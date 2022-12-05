e0dfa41fba081abb9249ce8071fb6324ee7fed33
sosso21 <sofianetop21.st@gmail.com>
2022-12-06 00:58:11 +0100

    :art: (feat): trait

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,24 @@
-#[derive(Debug)]
-struct Color(i32, i32, i32);
-
-#[derive(Debug)]
-struct Point(i32, i32, i32);
-
-#[derive(Debug)]
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
+trait HasArea {
+    fn area(&self) -> f64;
 }
 
-#[derive(Debug)]
-struct Person<'a> {
-    name: &'a str,
-    age: u8,
+struct Square {
+    x: f64,
+    y: f64,
 }
 
-impl User {
-    fn create(person: Person) -> User {
-        User {
-            active: false,
-            username: String::from(person.name),
-            email: String::from("myEmail@gmail.com"),
-            sign_in_count: 0,
-        }
+impl HasArea for Square {
+    fn area(&self) -> f64 {
+        self.x * self.y
     }
+}
 
-    // This method is used to introduce a user
-    fn introduction(&self) {
-        println!(
-            "Hello! My name is {} and My email is {}  .",
-            self.username, self.email
-        );
-    }
+fn print_area<T: HasArea>(square: T) {
+    let area = square.area();
+    println!(" the area is {area} ")
 }
 
 fn main() {
-    let user1 = User {
-        email: String::from("someone@example.com"),
-        username: String::from("someusername123"),
-        active: true,
-        sign_in_count: 1,
-    };
-
-    let black: Color = Color(0, 0, 0);
-    let origin: Point = Point(0, 0, 0);
-
-    let active = user1.active;
-    let email = user1.email;
-    let sign_in_count = user1.sign_in_count;
-
-    let name = "Moh";
-    let age = 27;
-    let moh = Person { name, age };
-
-    let moh_name = moh.name;
-    let moh_age = moh.age;
-
-    let change_user: User = User::create(moh);
-
-    change_user.introduction();
-
-    println!(
-        "black : {black:?}  origin : {origin:?}  , user1 : {} , active : {active} email : {email} sign_in_count : {sign_in_count} , moh : {moh_name} moh age {moh_age} ",
-        user1.username
-    );
+    let chess = Square { x: 5.0, y: 5.0 };
+    print_area(chess)
 }
